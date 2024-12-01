f327491430f1eac92db05bd4f9369aa5d48ecd83
imarcus14 <ivanmarcus1@gmail.com>
2024-12-01 17:41:14 -0500

    Add styling and the profile cards

---
+++ src/components/ProfileCards/ProfileCards.jsx
@@ -0,0 +1,78 @@
+import "./ProfileCards.scss";
+
+import { useEffect, useState } from "react";
+
+import Button from "../Button/Button";
+import { Link } from "react-router-dom";
+import axios from 'axios';
+import { motion } from "motion/react"
+
+const ProfileCards = () => {
+
+    const [userInfo, setUserInfo] = useState([]);
+    const [selectedCard, setSelectedCard] = useState(null);
+
+
+    const fetchUserInfo = async () => {
+        try{
+            const response = await fetch('../../../public/test.json')
+            const data = await response.json();
+            setUserInfo(data);
+        }catch(e){
+            console.error(e);
+        }
+    }
+
+    useEffect(() =>{
+        fetchUserInfo();
+    },[]);
+
+
+
+    return ( 
+        <div className="profile-card__container">
+            {userInfo.map((user) => (
+                    <motion.div 
+                    className="profile-card" 
+                    key={user.id}
+                    onClick={() => setSelectedCard((prev) => (prev === user.id ? null : user.id))}
+                    initial={{ scale: 1 }}
+                    animate={{
+                        scale: selectedCard === user.id ? 1.2 : 1, 
+                        zIndex: selectedCard === user.id ? 1 : 0, 
+                    }}
+                    whileHover={{ scale: selectedCard === user.id ? 1.2 : 1.1 }}
+                    transition={{ duration: 0.3 }}>
+                        <div className="profile-card__left">
+                            <img src={user.profileImage} alt="Other peoples profile images"/>
+                            <Link to="">
+                                <Button text="Chat"/>
+                            </Link>
+                        </div>
+                        <div className="profile-card__right">
+                            <p className="profile-card__info"> User: {user.username}</p>
+                            <p className="profile-card__info"> Dog's Name: {user.dogName}</p>
+                            <p className="profile-card__info"> Breed: {user.dogBreed}</p>
+                            <p className="profile-card__info"> Age:{user.dogAge}</p>
+                            <p className="profile-card__info"> Personality: {user.dogPersonality}</p>
+                            <p className="profile-card__info"> Location: {user.location}</p>
+                        </div>
+                    </motion.div>
+            ))}
+            {/* <div className="profile-card">
+                <img src="" alt="Other peoples profile images"/>
+                <div>
+                    <p className="profile-card__info"></p>
+                    <p className="profile-card__info"></p>
+                    <p className="profile-card__info"></p>
+                    <p className="profile-card__info"></p>
+                    <p className="profile-card__info"></p>
+                    <p className="profile-card__info"></p>
+                </div>
+            </div> */}
+            
+        </div>
+     );
+}
+ 
+export default ProfileCards;
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import "./MainPage.scss"
 
 import AccountContent from "../../components/AccountContent/AccountContent";
 import Header from "../../components/Header/Header";
+import ProfileCards from "../../components/ProfileCards/ProfileCards";
 
 const MainPage = () => {
     return ( 
@@ -9,6 +10,7 @@ const MainPage = () => {
         <Header/>
         {/* Need to add these props to component below: username, dogName, dogBreed, dogAge, dogPersonality, location */}
         <AccountContent/>
+        <ProfileCards/>
         </>
      );
 }
