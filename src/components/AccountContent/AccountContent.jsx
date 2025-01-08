da2f521146972671b6395089f3632ee6eb40c2e3
imarcus14 <ivanmarcus1@gmail.com>
2025-01-07 21:25:47 -0500

    Add tennis balls

--- src/components/AccountContent/AccountContent.jsx
+++ src/components/AccountContent/AccountContent.jsx
@@ -3,6 +3,7 @@ import "./AccountContent.scss";
 import { useEffect, useState } from "react";
 
 import axios from 'axios';
+import ball from "../../assets/icons/dog-ball.jpg"
 
 const AccountContent = () => {
 
@@ -32,17 +33,25 @@ const AccountContent = () => {
     const imageURL = !profile.dog.photo.startsWith('http')  ? `${URL}/${profile.dog.photo}` : profile.dog.photo;
     
     return ( 
-       <div className="account-content">
-            <img  className="account-content__photo" src={imageURL} alt={`${profile.dog.name} the dog`}/>
-            <div className="account-content__info">
-                <p className="account-content__specifics">Username: {profile.user.username}</p>
-                <p className="account-content__specifics">Dog's Name: {profile.dog.name}</p>
-                <p className="account-content__specifics">Breed: {profile.dog.breed}</p>
-                <p className="account-content__specifics">Dog Age: {profile.dog.age}</p>
-                <p className="account-content__specifics">Personality: {profile.dog.personality}</p>
-                <p className="account-content__specifics">City: {profile.user.city}</p>
-            </div>
-       </div> 
+        <div className="account">
+            <img className="account-content__ball-one" src={ball} alt="tennis ball"/>
+
+            <div className="account-content">
+                <img  className="account-content__photo" src={imageURL} alt={`${profile.dog.name} the dog`}/>
+                <div className="account-content__info">
+                    <p className="account-content__specifics">Username: {profile.user.username}</p>
+                    <p className="account-content__specifics">Dog's Name: {profile.dog.name}</p>
+                    <p className="account-content__specifics">Breed: {profile.dog.breed}</p>
+                    <p className="account-content__specifics">Dog Age: {profile.dog.age}</p>
+                    <p className="account-content__specifics">Personality: {profile.dog.personality}</p>
+                    <p className="account-content__specifics">City: {profile.user.city}</p>
+                </div>
+                {/* <img className="account_content__ball-one" src={ball} alt="tennis ball"/> */}
+                {/* <img className="account_content__ball-two" src={ball} alt="tennis ball"/> */}
+            </div> 
+            <img className="account-content__ball-two" src={ball} alt="tennis ball"/>
+
+       </div>
      );
 }
  
